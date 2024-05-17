1f81d2a86d4b1fe04bcdb5d4de03c9267c9d1729
Josue Igiraneza <igiranezaj28@gmail.com>
2024-05-17 11:47:56 +0200

    Study and course section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     fontFamily: {
       Lato: ['Lato', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        gold: "#a18a4d",
+        blacktext: "#212529",
+        lightGray: "#D5D5D5",
+        grey: "#333"  
+      }
+    },
   },
   plugins: [],
 }
